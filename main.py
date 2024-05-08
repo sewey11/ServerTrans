bcd6d6b736f693f8af074114dded25c464696754
sewey11 <133036731+sewey11@users.noreply.github.com>
2024-05-08 19:39:41 +0100

    Update main.py2

--- main.py
+++ main.py
@@ -60,4 +60,4 @@ app = web.Application()
 app.router.add_route('*', '/', handle)
 
 if __name__ == "__main__":
-    web.run_app(app, host='127.0.0.1', port=0000)
+    web.run_app(app, host='localhost', port=0000)
